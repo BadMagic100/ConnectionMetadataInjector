c7a3303262f0a2a7a6af4531e9631988df48ca54
Sean Dempsey <dempsey.sean@outlook.com>
2022-02-04 17:38:58 -0800

    Add project files.

---
+++ ConnectionMetadataInjector/ConnectionMetadataInjector.cs
@@ -0,0 +1,23 @@
+﻿using Modding;
+
+namespace ConnectionMetadataInjector
+{
+    public class ConnectionMetadataInjector : Mod
+    {
+        internal static ConnectionMetadataInjector? Instance;
+
+        public override string GetVersion() => GetType().Assembly.GetName().Version.ToString();
+
+        public ConnectionMetadataInjector() : base()
+        {
+            Instance = this;
+        }
+
+        public override void Initialize()
+        {
+            Log("Initializing");
+
+            Log("Initialized");
+        }
+    }
+}
